0cba795ee41641649c56a667a2208ff076eede3d
unkn8wn999-bit <unkn8wn999@gmail.com>
2025-09-16 13:45:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "marc123",
+    marc: "marc123",
   },
 };
 
